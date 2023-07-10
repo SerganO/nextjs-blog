27ebf8b114ce125c25c849a18169f3c8c26bf440
SerganO <sergeu.ostrovetskiu.2001@gmail.com>
2023-07-10 14:51:56 +0300

    redux setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
 import "../styles/global.css";
-import ErrorPage from 'next/error';
+import ErrorPage from "next/error";
 import { AppProps } from "next/app";
+import { Provider } from 'react-redux'
+import configureStore from 'reduxFunc/functions/configureStore'
+
+const store = configureStore(undefined)
 
 export default function App({ Component, pageProps }: AppProps) {
-
   if (pageProps.error) {
     return <ErrorPage statusCode={404} />;
   }
 
-
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
